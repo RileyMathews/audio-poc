2f9f290d4e24e9c4f477bf3fb39aaa94af898404
Riley Mathews <rileymathews80@gmail.com>
2020-06-30 21:16:02 -0500

    added super basic distortion effect

--- main.py
+++ main.py
@@ -3,31 +3,44 @@
 import pyaudio
 import wave
 import sys
+import os
 
-CHUNK = 1024
+def distort(frames):
+    amp_list = list(frames)
+    for i in range(len(amp_list)):
+        amp = amp_list[i]
+        if amp > 127:
+            amp_list[i] = 255
+        else:
+            amp_list[i] = 0
+    return bytes(amp_list)
 
-wf = wave.open("samples/chord.wav", 'rb')
+def play_file(name, effect=None):
+    wf = wave.open(f"samples/{name}", 'rb')
 
-# instantiate PyAudio (1)
-p = pyaudio.PyAudio()
+    # instantiate PyAudio (1)
+    p = pyaudio.PyAudio()
 
-# open stream (2)
-stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                channels=wf.getnchannels(),
-                rate=wf.getframerate(),
-                output=True)
+    # open stream (2)
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
 
-# read data
-data = wf.readframes(CHUNK)
+    all_frames = wf.readframes(wf.getnframes())
+    if effect != None:
+        all_frames = effect(all_frames)
+    stream.write(all_frames)
 
-# play stream (3)
-while len(data) > 0:
-    stream.write(data)
-    data = wf.readframes(CHUNK)
+    stream.stop_stream()
+    stream.close()
 
-# stop stream (4)
-stream.stop_stream()
-stream.close()
+    p.terminate()
 
-# close PyAudio (5)
-p.terminate()
+    wf.close()
+
+
+
+for file in os.listdir("./samples"):
+    print(file)
+    play_file(file, distort)
